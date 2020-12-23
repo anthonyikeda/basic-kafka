fda934734cfc416ee6ffa07b7c50e46ce28609be
Anthony Ikeda <anthony.ikeda@gmail.com>
2020-12-23 12:05:38 -0800

    Basic implementation for consuming Kafka done

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,44 @@
-use futures::StreamExt;
-use log::{info, warn};
+use rdkafka::{ClientConfig, Message};
+use rdkafka::consumer::{StreamConsumer, CommitMode};
+use rdkafka::consumer::Consumer;
+use tokio::stream::StreamExt;
+use std::str;
 
-use rdkafka::client::ClientContext;
-use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
-use rdkafka::consumer::stream_consumer::StreamConsumer;
-use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
-use rdkafka::error::KafkaResult;
-use rdkafka::message::{Headers, Message};
-use rdkafka::topic_partition_list::TopicPartitionList;
-use rdkafka::util::get_rdkafka_version;
-
-use crate::example_utils::setup_logger;
-
-mod example_utils;
-
-// A context can be used to change the behavior of producers and consumers by adding callbacks
-// that will be executed by librdkafka.
-// This particular context sets up custom callbacks to log rebalancing events.
-struct CustomContext;
-
-impl ClientContext for CustomContext {}
-
-impl ConsumerContext for CustomContext {
-    fn pre_rebalance(&self, rebalance: &Rebalance) {
-        info!("Pre rebalance {:?}", rebalance);
-    }
-
-    fn post_rebalance(&self, rebalance: &Rebalance) {
-        info!("Post rebalance {:?}", rebalance);
-    }
-
-    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
-        info!("Committing offsets: {:?}", result);
-    }
-}
-
-// A type alias with your custom consumer can be created for convenience.
-type LoggingConsumer = StreamConsumer<CustomContext>;
-
-async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
-    let context = CustomContext;
-
-    let consumer: LoggingConsumer = ClientConfig::new()
-        .set("group.id", group_id)
-        .set("bootstrap.servers", brokers)
-        .set("enable.partition.eof", "false")
-        .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "true")
-        //.set("statistics.interval.ms", "30000")
-        //.set("auto.offset.reset", "smallest")
-        .set_log_level(RDKafkaLogLevel::Debug)
-        .create_with_context(context)
+#[tokio::main]
+async fn main() {
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", "rust-group")
+        .set("bootstrap.servers", "localhost:9092")
+        .create()
         .expect("Consumer creation failed");
 
-    consumer
-        .subscribe(&topics.to_vec())
-        .expect("Can't subscribe to specified topics");
+    consumer.subscribe(&["rusty-msgs"])
+        .expect("Error consuming messages");
 
-    // consumer.start() returns a stream. The stream can be used ot chain together expensive steps,
-    // such as complex computations on a thread pool or asynchronous IO.
     let mut message_stream = consumer.start();
 
     while let Some(message) = message_stream.next().await {
         match message {
-            Err(e) => warn!("Kafka error: {}", e),
-            Ok(m) => {
-                let payload = match m.payload_view::<str>() {
-                    None => "",
-                    Some(Ok(s)) => s,
-                    Some(Err(e)) => {
-                        warn!("Error while deserializing message payload: {:?}", e);
-                        ""
-                    }
-                };
-                info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
-                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
-                if let Some(headers) = m.headers() {
-                    for i in 0..headers.count() {
-                        let header = headers.get(i).unwrap();
-                        info!("  Header {:#?}: {:?}", header.0, header.1);
+            Err(e) => println!("Error {}", e),
+            Ok(message) => {
+                match message.payload() {
+                    Some(payload) => {
+                        println!("{}", str::from_utf8(&payload).unwrap());
+                        let result = consumer.commit_message(&message, CommitMode::Async);
+                        match result {
+                            Ok(_result) => {
+                                println!("Committed to the queue!");
+                            },
+                            Err(_e) => {
+                                println!("No such commitment");
+                            }
+                        }
+                    },
+                    None => {
+                        println!("Empty message!");
                     }
                 }
-                consumer.commit_message(&m, CommitMode::Async).unwrap();
             }
         };
     }
-}
-
-#[tokio::main]
-async fn main() {
-    setup_logger(true, Some("rusty_log.txt"));
-
-    let (version_n, version_s) = get_rdkafka_version();
-    info!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
-
-    consume_and_print("localhost:9092", "sample_group", &["rusty-msgs"]).await
 }
